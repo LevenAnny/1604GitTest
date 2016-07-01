ef0f579918ed3fb46f7a23fcdbcda5a7b6acd369
Leven_sky <396072786@qq.com>
2016-07-01 14:27:46 +0800

    第一次提交项目
    第一次创建项目提交

---
+++ GitDemo/GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by Leven on 16/7/1.
+//  Copyright © 2016年 Leven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/GitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitDemo
+//
+//  Created by Leven on 16/7/1.
+//  Copyright © 2016年 Leven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
